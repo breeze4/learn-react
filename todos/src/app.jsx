370ad8a5e1c2ea9dab174e13a84d80f6d0b26171
breeze4 <ambiturner0@gmail.com>
2015-08-15 22:34:25 -0700

    saving todos to firebase

--- todos/src/app.jsx
+++ todos/src/app.jsx
@@ -1,12 +1,28 @@
 var React = require('react');
+var ReactFire = require('reactfire');
+var Firebase = require('firebase');
+var Header = require('./header');
 
-var Hello = React.createClass({
-  render: function() {
-    return <h1 className="red">
-      Hello!
-    </h1>
-  }
+var rootUrl = 'https://scorching-inferno-5880.firebaseio.com/';
+
+var App = React.createClass({
+    mixins: [ReactFire],
+    componentWillMount: function () {
+        // reactfire, sets 'items' as a state attribute that gets the firebase instance bound to it
+        this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
+        this.firebaseRefs.items.set
+    },
+    render: function () {
+        return <div className="row panel panel-default">
+            <div className="col-md-8 col-md-offset-2">
+                <h2 className="text-center">
+                    To-Do List
+                </h2>
+                <Header itemsStore={this.firebaseRefs.items} />
+            </div>
+        </div>
+    }
 });
 
-var element = React.createElement(Hello, {});
+var element = React.createElement(App, {});
 React.render(element, document.querySelector('.container'));
---
+++ todos/src/header.jsx
@@ -0,0 +1,38 @@
+var React = require('react');
+
+var Header = React.createClass({
+    getInitialState: function () {
+        return {
+            text: ""
+        }
+    },
+    handleClick: function () {
+        console.log("adding todo: ", this.state.text);
+        if (!this.state.text) return;
+        this.props.itemsStore.push({
+            text: this.state.text,
+            done: false
+        });
+        this.setState({text: ''});
+    },
+    handleInputChange: function (event) {
+        this.setState({
+            text: event.target.value
+        })
+    },
+    render: function () {
+        return <div className="input-group">
+            <input type="text" className="form-control" onChange={this.handleInputChange} value={this.state.text}/>
+            <span className="input-group-btn">
+                <button
+                    className="btn btn-default"
+                    type="button"
+                    onClick={this.handleClick}>
+                    Add
+                </button>
+            </span>
+        </div>
+    }
+});
+
+module.exports = Header;
